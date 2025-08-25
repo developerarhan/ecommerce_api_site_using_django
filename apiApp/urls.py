cb89352c86915242a34f0ddc392ea0dc22add8d1
developerarhan <khanarhan0205@gmail.com>
2025-08-25 20:14:52 +0530

    Final Project For Deployment

---
+++ apiApp/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('category_list', views.category_list, name='category_list'),
+    path('product_list', views.product_list, name='product_list'),
+    path('category/<slug:slug>', views.category_detail, name='category_detail'),
+    path('products/<slug:slug>', views.product_detail, name='product_detail'),
+    path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
+    path('update_cartitem_quantity/', views.update_cartitem_quantity, name='update_cartitem_quantity'),
+    path('add_review/', views.add_review, name='add_review'),
+    path('delete_cartitem/<int:pk>/', views.delete_cartitem, name='delete_cartitem'),
+    path('update_review/<int:pk>/', views.update_review, name='update_review'),
+    path('delete_review/<int:pk>/', views.delete_review, name='delete_review'),
+    path('add_to_wishlist/', views.add_to_wishlist, name='add_to_wishlist'),
+    path('search/', views.prodcut_search, name='search'),
+    path('create_checkout_session/', views.create_checkout_session, name='create_checkout_session'),
+    path('webhook/', views.my_webhook_view, name='webhook'),
+]
