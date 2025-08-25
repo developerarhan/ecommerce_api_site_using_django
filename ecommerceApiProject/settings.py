6087866cabe39f9e074ef5e3961c7e76e86c1404
developerarhan <khanarhan0205@gmail.com>
2025-08-25 21:04:16 +0530

    Fix: Postgresql settings.py

--- ecommerceApiProject/settings.py
+++ ecommerceApiProject/settings.py
@@ -82,28 +82,26 @@ WSGI_APPLICATION = 'ecommerceApiProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DB = os.getenv("DB")
+# DB = os.getenv("DB")
 #if we want use our project on production level then DB have value from env and otherwisemif we to use it on 
 # Development level then DB have no value.
-if not DB:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD': os.getenv("PG_PASSWORD"),
+        'HOST': os.getenv("PG_HOST"),
+        'PORT': os.getenv("PG_PORT"),
         }
     }
-
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME':'railway',
-            'USER':'postgres',
-            'PASSWORD': os.getenv("PG_PASSWORD"),
-            'HOST': os.getenv("PG_HOST"),
-            'PORT': os.getenv("PG_PORT"),
-            }
-        }
 
 
 # Password validation
